d9e823ba52ad48bc9fc95ca78133518c6b036bf9
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-11-18 09:55:30 +0800

    added the tabs

--- CKJY Final/ContentView.swift
+++ CKJY Final/ContentView.swift
@@ -9,11 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello!")
+        TabView {
+            RecipesView()
+                .tabItem {
+                    Label("Recipes", systemImage: "takeoutbag.and.cup.and.straw")
+                }
+            FoodListView()
+                .tabItem {
+                    Label("Food List", systemImage: "list.bullet.clipboard")
+                }
+            ProgressView()
+                .tabItem {
+                    Label("Progress", systemImage: "circle.circle")
+                }
         }
         .padding()
     }
---
+++ CKJY Final/ProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  ProgressView.swift
+//  CKJY Final
+//
+//  Created by Celine Quek on 18/11/23.
+//
+
+import SwiftUI
+
+struct ProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProgressView()
+}
