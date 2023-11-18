699c9d1f90a0318c6cd38785a78102057a5bf8df
Celine-ST <celine.quek10@rafflesgirlssch.edu.sg>
2023-11-18 11:01:23 +0800

    added a circular progress bar

--- CKJY Final/ProgressView.swift
+++ CKJY Final/ProgressView.swift
@@ -9,12 +9,36 @@ import SwiftUI
 
 struct ProgressView: View {
     
-    @State private var leaves = 0
+    @State var leaves = 0
+    @State var leafGoal = 10
     
     var body: some View {
         VStack {
+            ZStack {
+                Circle()
+                    .stroke( // 1
+                        Color.pink.opacity(0.5),
+                        lineWidth: 30
+                    )
+                Circle() // 2
+                    .trim(from: 0, to: CGFloat(leaves) / CGFloat(leafGoal)) // 1
+                    .stroke(Color.pink,
+                            style: StrokeStyle(
+                                lineWidth: 30,
+                                lineCap: .round
+                            )
+                    )
+                    .rotationEffect(.degrees(-90))
+                    .animation(.easeOut, value: CGFloat(leaves) / CGFloat(leafGoal))
+            }
+                .frame(width: 200, height: 200)
             HStack {
-                Stepper("\(leaves) times rickrolled", value: $leaves)
+                Stepper("\(leaves)", value: $leaves)
+                    .padding(20)
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(30)
+                Stepper("\(leafGoal)", value: $leafGoal)
                     .padding(20)
                     .background(.blue)
                     .foregroundColor(.white)
