ad652da384982ed66a2ace3399cd7d8f4712cc91
Yun Ye <yunye.koo10@rafflesgirlssch.edu.sg>
2023-11-18 09:51:56 +0800

    Initial Commit

---
+++ CKJY Final/CKJY_FinalApp.swift
@@ -0,0 +1,17 @@
+//
+//  CKJY_FinalApp.swift
+//  CKJY Final
+//
+//  Created by Koo  on 18/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CKJY_FinalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
